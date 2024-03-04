7f562a695eb8b5645ff99200117ccff4b13bec06
Gabremon <gwomack2003@gmail.com>
2024-03-04 09:37:53 -0800

    basic commit

--- src/BucketSortOptimized.java
+++ src/BucketSortOptimized.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class BucketSortOptimized {
+import java.util.ArrayList;
+
+public class BucketSortOptimized {
+    public static void main(String[] args) {
+        int[] A = {2, 5, 3, 1, 7, 4}; // Example input array
+        int n = A.length;
+
+        ArrayList<Integer>[] B = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            B[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            B[hashFunction(A[i], n)].add(A[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            insertionSort(B[i]);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            result.addAll(B[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    // Hash function to determine the index in B for each element of A
+    public static int hashFunction(int x, int n) {
+        return x * (n - 1) / (x + 1);
+    }
+
+    // Insertion sort
+    public static void insertionSort(ArrayList<Integer> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            int key = arr.get(i);
+            int j = i - 1;
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
+                j = j - 1;
+            }
+            arr.set(j + 1, key);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] A = {2, 5, 3, 1, 7, 4}; // Example input array
+        int n = A.length;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<Integer>[] B = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            B[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            B[hashFunction(A[i], n)].add(A[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            insertionSort(B[i]);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            result.addAll(B[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    // Hash function to determine the index in B for each element of A
+    public static int hashFunction(int x, int n) {
+        return x * (n - 1) / (x + 1);
+    }
+
+    // Insertion sort
+    public static void insertionSort(ArrayList<Integer> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            int key = arr.get(i);
+            int j = i - 1;
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
+                j = j - 1;
+            }
+            arr.set(j + 1, key);
         }
     }
 }
